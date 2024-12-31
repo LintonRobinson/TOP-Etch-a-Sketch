5315e2ea6cb0f515cdebd0b2297d699db9230f86
LintonRobinson <40444726+LintonRobinson@users.noreply.github.com>
2024-12-30 19:53:22 -0500

    Color randomization funtionality

--- js/main.js
+++ js/main.js
@@ -7,35 +7,63 @@ const container = document.querySelector('.container');
 //Start Button Element
 const startBtn = document.querySelector('#startBtn');
 
-// Create Default Grid Rows (16)
-for (i=1;i <= 16;i++) {
-    // The value of the input is the counter condition of the loop
-    const createEtchGridRow = document.createElement('div');
-    createEtchGridRow.classList.add('etch-grid-row');
-    container.appendChild(createEtchGridRow);
+// Latest Grid Size
+
+let mostRecentGridSize = 16
+
+// Default Grid Rows (16)
+function defaultGrid() {
+    for (i=1;i <= 16;i++) {
+        // The value of the input is the counter condition of the loop
+        const createEtchGridRow = document.createElement('div');
+        createEtchGridRow.classList.add('etch-grid-row');
+        container.appendChild(createEtchGridRow);
+    };
+    
+    // Create Default Grid Squares In Each Row (16)
+    const etchGridRowElement = document.querySelectorAll('.etch-grid-row');
+    etchGridRowElement.forEach(element => {
+        for (i=1;i <= 16;i++) {
+            let etchGrid = document.createElement('div');
+            etchGrid.classList.add('etch-grid');
+            element.appendChild(etchGrid);
+        };
+    });
+    
+    // Mouse Hover Changes Grid Background (Black)
+    let generatedEtchGrid = document.querySelectorAll('.etch-grid')
+    generatedEtchGrid.forEach(element => {
+        element.addEventListener('mouseover', () => {
+            element.style.backgroundColor = "Black";
+        })
+    });
 };
 
-// Create Default Grid Squares In Each Row (16)
-const etchGridRowElement = document.querySelectorAll('.etch-grid-row');
-etchGridRowElement.forEach(element => {
-    for (i=1;i <= 16;i++) {
-        const etchGrid = document.createElement('div');
-        etchGrid.classList.add('etch-grid');
-        element.appendChild(etchGrid);
+function mostRecentGrid() {
+    for (i=1;i <= mostRecentGridSize;i++) {
+        // The value of the input is the counter condition of the loop
+        const createEtchGridRow = document.createElement('div');
+        createEtchGridRow.classList.add('etch-grid-row');
+        container.appendChild(createEtchGridRow);
     };
-});
+    
+    // Create Default Grid Squares In Each Row (16)
+    const etchGridRowElement = document.querySelectorAll('.etch-grid-row');
+    etchGridRowElement.forEach(element => {
+        for (i=1;i <= mostRecentGridSize;i++) {
+            const etchGrid = document.createElement('div');
+            etchGrid.classList.add('etch-grid');
+            element.appendChild(etchGrid);
+        };
+    });
+};
 
-// Mouse Hover Changes Grid Background
-let generatedEtchGrid = document.querySelectorAll('.etch-grid')
-generatedEtchGrid.forEach(element => {
-    element.addEventListener('mouseover', () => {
-        element.style.backgroundColor = "Black";
-    })
-});
+
+defaultGrid()
 
 
  
-function removeDefaultGrid () {
+function removeDefaultGrid() {
     const etchGrid = document.querySelectorAll('.etch-grid')
     const etchGridRow = document.querySelectorAll('.etch-grid-row');
     etchGrid.forEach(element => element.remove());
@@ -45,6 +73,7 @@ function removeDefaultGrid () {
 function createUserGrid() {
     // Store User Grid Number 
     let userGridInput = Number(document.querySelector('#userGridInput').value);
+    mostRecentGridSize = userGridInput
     // Create Grid Rows Based off of User Grid Number
     for (i=1;i <= userGridInput;i++) {
         const createEtchGridRow = document.createElement('div');
@@ -61,7 +90,7 @@ function createUserGrid() {
          };
      });
     // Mouse Hover Changes Grid Background
-    let generatedEtchGrid = document.querySelectorAll('.etch-grid');
+    const generatedEtchGrid = document.querySelectorAll('.etch-grid');
     generatedEtchGrid.forEach(element => {
         element.addEventListener('mouseover', () => {
             element.style.backgroundColor = "Black"
@@ -86,6 +115,63 @@ startBtn.addEventListener('click' , () => {
     
 });
 
+// Clear Grid Background Color(s)
+function clearGrid() {
+    removeDefaultGrid();
+    mostRecentGrid()
+};
+
 
 // Toggle Random Colors 
+const colorToggleButton = document.querySelector('#colorToggleButton');
+
+
+// Generate Random Color
+function generateRandomColor() {
+    const letters = "0123456789ABCDEF"
+    let randomColor = ["#"];
+    for (let i = 0 ; i<6 ; i++) {
+        randomColor += letters[Math.floor(Math.random() *16)]
+    };
+    console.log(randomColor);
+    return randomColor;
+};
+
+colorToggleButton.addEventListener('click' , () => {
+    
+    if (colorToggleButton.classList.contains('toRandomColor')) {
+        clearGrid();
+        addRandomGridColorListeners();
+        colorToggleButton.classList.add('toBlack');
+        colorToggleButton.classList.remove('toRandomColor');
+        colorToggleButton.textContent = "To Black"
+    } else {
+        clearGrid();
+        addBlackGridColorListeners();
+        colorToggleButton.classList.add('toRandomColor');
+        colorToggleButton.classList.remove('toBlack');
+        colorToggleButton.textContent = "To Random Color"
+    };
+})
+
+
+function addBlackGridColorListeners() {
+    const generatedEtchGrid = document.querySelectorAll('.etch-grid');
+    generatedEtchGrid.forEach(element => {
+        element.addEventListener('mouseover', () => {
+            element.style.backgroundColor = "Black";
+        })
+    });
+};
+
+function addRandomGridColorListeners() {
+    const generatedEtchGrid = document.querySelectorAll('.etch-grid');
+    generatedEtchGrid.forEach(element => {
+        element.addEventListener('mouseover', () => {
+            element.style.backgroundColor = generateRandomColor();
+        })
+    });
+};
+
+
 
